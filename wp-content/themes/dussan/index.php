a2b4baf194cf97acd51a27d72d5b5caa22abb05c
Dussan server <git@8manos.com>
2012-03-06 20:58:10 -0600

    headers y bug fixes

--- wp-content/themes/dussan/index.php
+++ wp-content/themes/dussan/index.php
@@ -6,15 +6,21 @@
 		<section class="slide" id="page1">
 
 			<article <?php post_class('percent-66'); ?> style="background-color:#D3D3D3">
-				<h1>PAGE 1 - 66%</h1>
+				<div class="article_header">
+					<h1>PAGE 1</h1>
+				</div>
 			</article>
 
 			<article <?php post_class('percent-34'); ?> style="background-color:#A7A6A6">
-				<h1>34%</h1>
+				<div class="article_header">
+					<h1>Titulo</h1>
+				</div>
 			</article>
 
 			<article <?php post_class('percent-34'); ?> style="background-color:#918F90">
-				<h1>34%</h1>
+				<div class="article_header">
+					<h1>Titulo</h1>
+				</div>
 			</article>
 
 		</section>
@@ -22,15 +28,21 @@
 		<section class="slide" id="page2">
 
 			<article <?php post_class('percent-66'); ?> style="background-color:#D3D3D3">
-				<h1>PAGE 2 - 66%</h1>
+				<div class="article_header">
+					<h1>Titulo 2</h1>
+				</div>
 			</article>
 
 			<article <?php post_class('percent-34'); ?> style="background-color:#A7A6A6">
-				<h1>34%</h1>
+				<div class="article_header">
+					<h1>Titulo</h1>
+				</div>
 			</article>
 
 			<article <?php post_class('percent-34'); ?> style="background-color:#918F90">
-				<h1>34%</h1>
+				<div class="article_header">
+					<h1>Titulo</h1>
+				</div>
 			</article>
 
 		</section>
@@ -38,15 +50,21 @@
 		<section class="slide" id="page3">
 
 			<article <?php post_class('percent-66'); ?> style="background-color:#D3D3D3">
-				<h1>PAGE 3 - 66%</h1>
+				<div class="article_header">
+					<h1>Titulo 3</h1>
+				</div>
 			</article>
 
 			<article <?php post_class('percent-34'); ?> style="background-color:#A7A6A6">
-				<h1>34%</h1>
+				<div class="article_header">
+					<h1>Titulo</h1>
+				</div>
 			</article>
 
 			<article <?php post_class('percent-34'); ?> style="background-color:#918F90">
-				<h1>34%</h1>
+				<div class="article_header">
+					<h1>Titulo</h1>
+				</div>
 			</article>
 
 		</section>
@@ -54,15 +72,65 @@
 		<section class="slide" id="page4">
 
 			<article <?php post_class('percent-66'); ?> style="background-color:#D3D3D3">
-				<h1>PAGE 4 - 66%</h1>
+				<div class="article_header">
+					<h1>Titulo 4</h1>
+				</div>
 			</article>
 
 			<article <?php post_class('percent-34'); ?> style="background-color:#A7A6A6">
-				<h1>34%</h1>
+				<div class="article_header">
+					<h1>Titulo</h1>
+				</div>
 			</article>
 
 			<article <?php post_class('percent-34'); ?> style="background-color:#918F90">
-				<h1>34%</h1>
+				<div class="article_header">
+					<h1>Titulo</h1>
+				</div>
+			</article>
+
+		</section>
+
+		<section class="slide" id="page5">
+
+			<article <?php post_class('percent-66'); ?> style="background-color:#D3D3D3">
+				<div class="article_header">
+					<h1>Titulo 5</h1>
+				</div>
+			</article>
+
+			<article <?php post_class('percent-34'); ?> style="background-color:#A7A6A6">
+				<div class="article_header">
+					<h1>Titulo</h1>
+				</div>
+			</article>
+
+			<article <?php post_class('percent-34'); ?> style="background-color:#918F90">
+				<div class="article_header">
+					<h1>Titulo</h1>
+				</div>
+			</article>
+
+		</section>
+
+		<section class="slide" id="page6">
+
+			<article <?php post_class('percent-66'); ?> style="background-color:#D3D3D3">
+				<div class="article_header">
+					<h1>Titulo 6</h1>
+				</div>
+			</article>
+
+			<article <?php post_class('percent-34'); ?> style="background-color:#A7A6A6">
+				<div class="article_header">
+					<h1>Titulo</h1>
+				</div>
+			</article>
+
+			<article <?php post_class('percent-34'); ?> style="background-color:#918F90">
+				<div class="article_header">
+					<h1>Titulo</h1>
+				</div>
 			</article>
 
 		</section>
